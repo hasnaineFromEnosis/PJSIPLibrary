b8d7177f265d76d1acf3ec19de6028b001ebb145
Shahwat Hasnaine <shahwat.hasnaine@enosisbd.com>
2024-07-09 16:04:46 +0600

    Updated package for necessary dependency and targets

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "PJSIPLibrary",
+    platforms: [
+        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -15,9 +18,18 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PJSIPLibrary"),
+                name: "Pjsua2",
+                path: "Sources/Pjsua2",
+                publicHeadersPath: "include"
+        ),
+        .target(
+            name: "PJSIPLibrary",
+            dependencies: ["Pjsua2"]
+        ),
         .testTarget(
             name: "PJSIPLibraryTests",
             dependencies: ["PJSIPLibrary"]),
+        
+        
     ]
 )
