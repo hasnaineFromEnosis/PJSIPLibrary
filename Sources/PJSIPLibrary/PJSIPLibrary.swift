05732d93264a7afdd970adaa1a7d6bf3f4ab7440
Shahwat Hasnaine <shahwat.hasnaine@enosisbd.com>
2024-07-10 21:05:10 +0600

    Implemented necessary delegate and method for pjsip library

--- Sources/PJSIPLibrary/PJSIPLibrary.swift
+++ Sources/PJSIPLibrary/PJSIPLibrary.swift
@@ -4,9 +4,65 @@
 import SwiftUI
 import Pjsua2
 
+protocol PJSIPLibraryDelegate: AnyObject {
+    func incomingCallReceived(callId: String)
+    
+    func acccountUpdateReceived(status: Bool)
+    
+    func callStatusRecieved(status: Int)
+}
 
-public class PJSIPLibrary: ObservableObject {
-    func x() {
+public struct PJSIPLibrary {
+    
+    // Delegate property
+    private weak var delegate: PJSIPLibraryDelegate?
+    
+    init() {
+        CPPWrapper().createLibWrapper()
+    }
+
+    func answerCall() {
         CPPWrapper().answerCall()
     }
+    
+    func hangupCall() {
+        CPPWrapper().hangupCall()
+    }
+    
+    func holdCall() {
+        CPPWrapper().holdCall()
+    }
+    
+    func unholdCall() {
+        CPPWrapper().unholdCall()
+    }
+    
+    func getIncomingCallId() -> String {
+       return CPPWrapper().incomingCallInfoWrapper()
+    }
+    
+    func call(uri: String) {
+        CPPWrapper().outgoingCall(uri)
+    }
+    
+    func login(username: String, password: String, domain: String, port: String = "5060") {
+        CPPWrapper().createAccountWrapper(username, password, domain, port);
+    }
+    
+    func logout() {
+        CPPWrapper().unregisterAccountWrapper()
+    }
+    
+    private func incoming_call_swift() {
+        self.delegate?.incomingCallReceived(callId: self.getIncomingCallId())
+    }
+    
+    private func acc_listener_swift(status: Bool) {
+        self.delegate?.acccountUpdateReceived(status: status)
+    }
+    
+    private func call_status_listener_swift ( call_answer_code: Int32) {
+        self.delegate?.callStatusRecieved(status: Int(call_answer_code))
+    }
+    
 }
