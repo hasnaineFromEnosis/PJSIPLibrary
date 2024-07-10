e94488be12fce1409c1f5dd4d441cf2ab57b5d33
Shahwat Hasnaine <shahwat.hasnaine@enosisbd.com>
2024-07-11 01:37:49 +0600

    Added systemlibrary in the package configuration

--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -9,42 +7,49 @@ let package = Package(
         .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PJSIPLibrary",
-            targets: ["PJSIPLibrary"]),
+            targets: ["PJSIPLibrary"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        .systemLibrary(
+            name: "CPJSIP",
+            pkgConfig: "libpjproject",
+            providers: [
+                .brew(["pjproject"]),
+                .apt(["libpjproject-dev"])
+            ]
+        ),
         .target(
             name: "PJSIPLibrary",
             dependencies: ["Pjsua2"]
         ),
         .target(
-                name: "Pjsua2",
-                path: "Sources/Pjsua2",
-                publicHeadersPath: "include",
-                cSettings: [
-                    .headerSearchPath("../LibraryFile/PjsipLibrary/pjsip/include"),
-                    .headerSearchPath("../LibraryFile/PjsipLibrary/pjlib/include"),
-                    .headerSearchPath("../LibraryFile/PjsipLibrary/pjlib-util/include"),
-                    .headerSearchPath("../LibraryFile/PjsipLibrary/pjmedia/include"),
-                    .headerSearchPath("../LibraryFile/PjsipLibrary/pjnath/include"),
-                    .headerSearchPath("../LibraryFile/PjsipLibrary/third_party/include"),
-                    .headerSearchPath("../LibraryFile/Libraries"),
-                    .define("PJ_AUTOCONF=1", to: "GCC_PREPROCESSOR_DEFINITIONS"),
-                ],
-                cxxSettings: [
-                    .headerSearchPath("../LibraryFile/PjsipLibrary/pjsip/include"),
-                    .headerSearchPath("../LibraryFile/PjsipLibrary/pjlib/include"),
-                    .headerSearchPath("../LibraryFile/PjsipLibrary/pjlib-util/include"),
-                    .headerSearchPath("../LibraryFile/PjsipLibrary/pjmedia/include"),
-                    .headerSearchPath("../LibraryFile/PjsipLibrary/pjnath/include"),
-                    .headerSearchPath("../LibraryFile/PjsipLibrary/third_party/include"),
-                    .headerSearchPath("../LibraryFile/Libraries"),
-                    .define("PJ_AUTOCONF=1", to: "GCC_PREPROCESSOR_DEFINITIONS"),
-                ]
+            name: "Pjsua2",
+            dependencies: ["CPJSIP"],
+            path: "Sources/Pjsua2",
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath("../LibraryFile/PjsipLibrary/pjsip/include"),
+                .headerSearchPath("../LibraryFile/PjsipLibrary/pjlib/include"),
+                .headerSearchPath("../LibraryFile/PjsipLibrary/pjlib-util/include"),
+                .headerSearchPath("../LibraryFile/PjsipLibrary/pjmedia/include"),
+                .headerSearchPath("../LibraryFile/PjsipLibrary/pjnath/include"),
+                .headerSearchPath("../LibraryFile/PjsipLibrary/third_party/include"),
+                .headerSearchPath("../LibraryFile/Libraries"),
+                .define("PJ_AUTOCONF=1", to: "GCC_PREPROCESSOR_DEFINITIONS"),
+            ],
+            cxxSettings: [
+                .headerSearchPath("../LibraryFile/PjsipLibrary/pjsip/include"),
+                .headerSearchPath("../LibraryFile/PjsipLibrary/pjlib/include"),
+                .headerSearchPath("../LibraryFile/PjsipLibrary/pjlib-util/include"),
+                .headerSearchPath("../LibraryFile/PjsipLibrary/pjmedia/include"),
+                .headerSearchPath("../LibraryFile/PjsipLibrary/pjnath/include"),
+                .headerSearchPath("../LibraryFile/PjsipLibrary/third_party/include"),
+                .headerSearchPath("../LibraryFile/Libraries"),
+                .define("PJ_AUTOCONF=1", to: "GCC_PREPROCESSOR_DEFINITIONS"),
+            ]
         ),
         .testTarget(
             name: "PJSIPLibraryTests",
