8ab6c87a407e2841098712584e2a0c2a8a79aa63
Shahwat Hasnaine <shahwat.hasnaine@enosisbd.com>
2024-07-11 21:59:31 +0600

    Added system library in package file

--- Package.swift
+++ Package.swift
@@ -13,6 +13,14 @@ let package = Package(
         ),
     ],
     targets: [
+        .systemLibrary(
+            name: "CPJSIP",
+            pkgConfig: "libpjproject",
+            providers: [
+                .brew(["pjproject"]),
+                .apt(["libpjproject-dev"])
+            ]
+        ),
         .target(
             name: "PJSIPLibrary",
             dependencies: ["Pjsua2"]
